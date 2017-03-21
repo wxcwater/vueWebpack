4be13496167a359a2b5008abfbd8baaa0d03aea7
wxcwater <245605794@qq.com>
2017-03-21 17:44:13 +0800

    水量主页模块

---
+++ vueWebpack/src/slMod/slMod_mainState.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="mainStateContainer">
+    <div v-for="component in currentDataViews" >
+      <component :is="component.c" :echarts="echarts"></component>
+    </div>
+  </div>
+</template>
+<script>
+import thsw from './mainStatus_thsw.vue'
+import hhhl from './mainStatus_hhhl.vue'
+import ygshd from './mainStatus_ygshd.vue'
+import ycj from './mainStatus_ycj.vue'
+import yhzw from './mainStatus_yhzw.vue'
+import qsyh from './mainStatus_qsyh.vue'
+
+export default {
+  name: 'slmodmainstate',
+  props: ['echarts','addComponents'],
+  components: {
+    // <my-component> 将只在父模板可用
+    'thsw':thsw,
+    'hhhl': hhhl,
+    'ygshd': ygshd,
+    'ycj': ycj,
+    'yhzw': yhzw,
+    'qsyh': qsyh
+  },
+  data:function(){
+    let currentDataViews = [];
+    currentDataViews = Array.from(new Set(this.addCompontents));
+    return{
+        'currentDataViews':currentDataViews
+    }
+  },
+  mounted:function(){
+
+  },
+  methods:{
+    changeDataView:function(newViews){
+        let currentDataViews = new Set(newViews);
+        this.currentDataViews = Array.from(currentDataViews);
+    }
+  }
+}
+
+</script>
+
+<style>
+  .mainStateContainer{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: stretch;
+  }
+  .contentComponent{
+
+  }
+</style>
